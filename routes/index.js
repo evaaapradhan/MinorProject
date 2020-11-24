0a77bbbf68932b7b4cb1c07e44b88c066fbd93a2
evaaapradhan <evasanapradhan@gmail.com>
2020-11-24 16:15:46 +0545

    pages linked

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('homepage', { title: 'ParkEasy' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.get('/booking', function(req, res, next) {
+  res.render('booking', { title: 'Book your space' });
 });
 
 module.exports = router;
